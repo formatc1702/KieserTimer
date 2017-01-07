b72ad6e7e8fc00531643d375b60ae025189a9dec
Daniel Rojas <github@danielrojas.net>
2017-01-07 17:58:39 +0100

    First working proof of concept

--- Firmware/src/main.cpp
+++ Firmware/src/main.cpp
@@ -4,15 +4,57 @@
 #define PIN_LED_RED 7
 #define PIN_LED_GRN 8
 
+#define TIME_GO_UP     4000
+#define TIME_WAIT_UP   2250
+#define TIME_GO_DOWN   4000
+#define TIME_WAIT_DOWN 2250
+
+#define NUM_REPETITIONS 10
+
+#define DELAY_BLIP 50
+
+void blip(int pin, int times) {
+  for (int i = 0; i < times; i++) {
+    digitalWrite(pin,HIGH);
+    delay(DELAY_BLIP);
+    digitalWrite(pin,LOW);
+    delay(DELAY_BLIP);
+  }
+}
+
+void sigGoUp() {
+  blip(PIN_LED_GRN, 1);
+}
+
+void sigGoDown() {
+  blip(PIN_LED_RED, 1);
+}
+
+void sigWaitUp() {
+  blip(PIN_LED_GRN, 2);
+}
+
+void sigWaitDown() {
+  blip(PIN_LED_RED, 2);
+}
+
 void setup() {
   pinMode(PIN_BUZZER,OUTPUT);
   pinMode(PIN_LED_RED,OUTPUT);
   pinMode(PIN_LED_GRN,OUTPUT);
+
+  for (int i = 0; i < NUM_REPETITIONS; i++) {
+    sigGoUp();
+    delay(TIME_GO_UP);
+    sigWaitUp();
+    delay(TIME_WAIT_UP);
+    sigGoDown();
+    delay(TIME_GO_DOWN);
+    sigWaitDown();
+    delay(TIME_WAIT_DOWN);
+  }
 }
 
 void loop() {
-  digitalWrite(PIN_LED_GRN,HIGH);
-  delay(100);
-  digitalWrite(PIN_LED_GRN,LOW);
-  delay(100);
+  while(true);
 }
